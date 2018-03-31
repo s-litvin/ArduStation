060f83b768ae55d7964fde6dfc2bd91b1a45cb14
S.Litvin <litvin.xx@gmail.com>
2018-03-31 19:14:22 +0300

    upd

--- AP___Client/AP___Client.ino
+++ AP___Client/AP___Client.ino
@@ -82,7 +82,7 @@ void handleRoot() {
 
 void handleJs() {
   page_content = "";
-  page_content += "<script src=\"https://code.jquery.com/jquery-3.3.1.min.js\" integrity=\"sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=\" crossorigin=\"anonymous\"></script><script src=\"https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.6.0/p5.js\"></script><script src=\"https://github.com/shpikyliak/robot/blob/master/robot.js\" type=\"text/javascript\"></script>";
+  page_content += "<script src=\"https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.6.0/p5.js\"></script><script src=\"https://cdn.rawgit.com/s-litvin/test/master/index.js\"></script>";
   sendPage();
 }
 
--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-function t() {alert('OK');}
+function setup() {
+    createCanvas(600, 450);
+    background(0, 200, 0);
+
+}
+
+function draw() {
+
+    ellipse(width/2, height/2, 50, 50);
+
+}
